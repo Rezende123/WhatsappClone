f9c3918ff3eedf7a327845c5c0db6f356ba14045
Felipe Rezende <feliperezende.srz@gmail.com>
2020-05-08 16:37:40 -0300

    Adicionando toolbar

--- app/src/main/java/com/curso/whatsappclone/activity/MainActivity.java
+++ app/src/main/java/com/curso/whatsappclone/activity/MainActivity.java
@@ -1,9 +1,14 @@
 package com.curso.whatsappclone.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,26 +18,53 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnExit;
-    FirebaseAuth auth;
+    private FirebaseAuth auth;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnExit = (Button) findViewById(R.id.btnExit);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        configToolbar();
+    }
 
-        btnExit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                auth = FirebaseConfig.getFirebaseAuth();
-                auth.signOut();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
 
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+        inflater.inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.item_singnOut:
+                singnOut();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
+    }
+
+    private void configToolbar() {
+        toolbar.setTitle("Whatsapp");
+        setSupportActionBar(toolbar);
+    }
+
+    private void singnOut() {
+        auth = FirebaseConfig.getFirebaseAuth();
+        auth.signOut();
+
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
