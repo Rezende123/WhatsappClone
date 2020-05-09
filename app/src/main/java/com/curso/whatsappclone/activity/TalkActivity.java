e0dd7b1a3b1c4b0561ba2b401597ed94bca2f44b
Felipe Rezende <feliperezende.srz@gmail.com>
2020-05-09 13:46:06 -0300

    Salvando mensagens

--- app/src/main/java/com/curso/whatsappclone/activity/TalkActivity.java
+++ app/src/main/java/com/curso/whatsappclone/activity/TalkActivity.java
@@ -4,22 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.curso.whatsappclone.R;
+import com.curso.whatsappclone.config.FirebaseConfig;
 import com.curso.whatsappclone.model.Contact;
+import com.curso.whatsappclone.model.Message;
 import com.curso.whatsappclone.services.Base64Service;
+import com.curso.whatsappclone.services.PreferenceService;
+import com.google.firebase.database.DatabaseReference;
 
 public class TalkActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private Contact contact;
 
+    private EditText editMessage;
+    private ImageButton sendMessage;
+
+    private String idUserSender;
+
+    private DatabaseReference firebase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_talk);
 
+        editMessage = (EditText) findViewById(R.id.edit_message);
+        sendMessage = (ImageButton) findViewById(R.id.send_message);
+
         Bundle extras = getIntent().getExtras();
+
+        PreferenceService preferenceService = new PreferenceService(TalkActivity.this);
+        idUserSender = preferenceService.getUserId();
 
         if (extras != null) {
             String userId = Base64Service.code(extras.getString("email"));
@@ -28,11 +50,42 @@ public class TalkActivity extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.toolbar_talk);
         configToolbar();
+
+        sendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textMessage = editMessage.getText().toString();
+
+                if (textMessage.isEmpty()) {
+                    Toast.makeText(TalkActivity.this, "Digite uma mensagem para ser enviada", Toast.LENGTH_LONG).show();
+                } else {
+                    Message message = new Message(idUserSender, textMessage);
+
+                    saveMessage(message);
+
+                    editMessage.setText("");
+                }
+            }
+        });
     }
 
     private void configToolbar() {
         toolbar.setTitle(contact.getName());
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
         setSupportActionBar(toolbar);
+    }
+
+    private boolean saveMessage(Message message) {
+        try {
+            firebase = FirebaseConfig.getFirebase().child("messages");
+            String idUserReceiver = contact.getUserId();
+
+            firebase.child(idUserSender).child(idUserReceiver).push().setValue(message);
+
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
---
+++ app/src/main/java/com/curso/whatsappclone/model/Message.java
@@ -0,0 +1,31 @@
+package com.curso.whatsappclone.model;
+
+public class Message {
+
+    private String userId;
+    private String message;
+
+    public Message() {
+    }
+
+    public Message(String userId, String message) {
+        this.userId = userId;
+        this.message = message;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
