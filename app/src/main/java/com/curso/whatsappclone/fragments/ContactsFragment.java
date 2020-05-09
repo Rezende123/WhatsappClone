54a70146d04a64682a5d578ba1a807aab7cf980f
Felipe Rezende <feliperezende.srz@gmail.com>
2020-05-08 21:48:50 -0300

    Exibindo um listView com os contatos salvos no firebase

--- app/src/main/java/com/curso/whatsappclone/fragments/ContactsFragment.java
+++ app/src/main/java/com/curso/whatsappclone/fragments/ContactsFragment.java
@@ -2,18 +2,36 @@ package com.curso.whatsappclone.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.curso.whatsappclone.R;
+import com.curso.whatsappclone.config.FirebaseConfig;
+import com.curso.whatsappclone.model.Contact;
+import com.curso.whatsappclone.services.PreferenceService;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ContactsFragment extends Fragment {
+
+    private ListView listView;
+    private ArrayAdapter adapter;
+    private ArrayList<String> contacts;
+
+    private DatabaseReference firebase;
 
     public ContactsFragment() {
         // Required empty public constructor
@@ -24,6 +42,46 @@ public class ContactsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contacts, container, false);
+        contacts = new ArrayList<>();
+
+        View view = inflater.inflate(R.layout.fragment_contacts, container, false);
+
+        listView = view.findViewById(R.id.lv_contacts);
+        adapter = new ArrayAdapter(
+                getActivity(),
+                R.layout.list_contact,
+                contacts
+        );
+        listView.setAdapter(adapter);
+
+        getContacts();
+
+        return view;
+    }
+
+    private void getContacts() {
+        PreferenceService preferenceService = new PreferenceService(getActivity());
+        String userAuthId = preferenceService.getUserId();
+
+        firebase = FirebaseConfig.getFirebase()
+                .child("contacts").child(userAuthId);
+
+        firebase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                contacts.clear();
+
+                for (DataSnapshot data: dataSnapshot.getChildren()) {
+                    Contact contact = data.getValue(Contact.class);
+                    contacts.add(contact.getName());
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
